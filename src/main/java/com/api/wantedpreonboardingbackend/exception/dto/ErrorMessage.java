5baed66c1b25d8416e6f96bb1adea7dcc9bd1519
jhva <junghun8158@naver.com>
2023-10-11 20:12:47 +0900

    :sparkles: feat: company Exception handler add

--- src/main/java/com/api/wantedpreonboardingbackend/exception/dto/ErrorMessage.java
+++ src/main/java/com/api/wantedpreonboardingbackend/exception/dto/ErrorMessage.java
@@ -10,7 +10,8 @@ import lombok.RequiredArgsConstructor;
 public enum ErrorMessage {
 
     CONFLICT_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버와 연결이 끊어졌습니다."),
-    NOT_EXIST_JOB(HttpStatus.NOT_FOUND, "채용공고가 존재하지 않습니다.");
+    NOT_EXIST_JOB(HttpStatus.NOT_FOUND, "채용공고가 존재하지 않습니다."),
+    NOT_EXIST_COMPANY(HttpStatus.NOT_FOUND, "해당 회사가 존재하지 않습니다.");
 
     private final HttpStatus status;
     private final String description;
